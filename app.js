a8c34ce7fd50f465219c694702cfd2334403ee30
Matvey Mayorov <mayorovmatvey1@gmail.com>
2021-07-08 01:15:10 +0300

    next step is to set hbs templates

--- app.js
+++ app.js
@@ -3,22 +3,31 @@ const express = require('express')
 const cors = require('cors')
 const http = require('http')
 const bodyParser= require('body-parser')
+const morgan = require('morgan')
 const middlewares = require('./middlewares.js')
 
 const app = express()
 const server = http.createServer(app)
-
+//const logger = morgan(':method :url :status :res[content-length] - :response-time ms')
 const PORT = process.env.PORT ?? 3000
 
-app.use(cors({
-    origin: 'localhost'
-}))
+app.set("view engine", "hbs")
 
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(cors())
 app.use(express.static('static'))
-app.use(middlewares.logger)
+app.use(morgan('combined'))
+//app.use(middlewares.logger)
+
 
 app.get('/', (req,res) => {
     res.sendFile(path.resolve(__dirname, 'static', 'index.html'))
+    console.log(process.env)
+})
+
+app.get('/favicon.ico', (req,res) => {
+    res.sendFile(path.resolve(__dirname, 'static', 'favicon.ico'))
 })
 
 app.get('/about', (req,res) => {
@@ -27,6 +36,13 @@ app.get('/about', (req,res) => {
 
 app.get('/profile', (req,res) => {
     res.sendFile(path.resolve(__dirname, 'static', 'profile.html'))
+})
+
+app.get('/:name', (req, res) => {
+    let name = req.params.name
+    res.json({
+        message: `Hello ${name}`
+    })
 })
 
 server.listen(PORT, () => {
--- middlewares.js
+++ middlewares.js
@@ -1,11 +1,11 @@
 const colors = require('colors')
-const chalk = require('chalk')
-const fs = require('fs')
+
 
 module.exports.logger = (req,res,next) => {
     const today = new Date()
+    const log = console.log
     const time = `Request ${today.getHours()}:${today.getMinutes()}:${today.getSeconds()}:${today.getMilliseconds()}`
-    const log = `${req.ip} ${req.protocol} ${req.method} ${req.path}`
-    console.log(colors.bgGreen.black(`${time} ${log}`))
+    const info = `${req.ip} ${req.protocol} ${req.method} ${req.path}`
+    log(colors.bgGreen.black(`Mine  ${time} ${info}`))
     next()
 }
