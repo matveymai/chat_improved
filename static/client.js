ca5afa67869c44f7125a8bbdf8a063df97a6e180
Matvey Mayorov <mayorovmatvey1@gmail.com>
2021-07-16 18:53:21 +0300

    output became more pretty

--- static/client.js
+++ static/client.js
@@ -1,17 +1,20 @@
-const socket = io()
+const socket = io() //создаем объект сокета
 
-const messages = document.getElementById('messages')
-const frm = document.getElementById('form')
-const input = document.getElementById('text')
+const messages = document.getElementById('messages') //элемент контейнера сообщений
+const frm = document.getElementById('form') //элемент формы ввода
+const input = document.getElementById('text') //элемент поля ввода текста
 
+//функция добавления новых сообщений в чате
 const addMessage = (message) => {
-    const div = document.createElement('div')
-    div.textContent = message
-    messages.appendChild(div)
-    messages.scrollTop = messages.scrollHeight
+    const div = document.createElement('div') //создаем контейнер для одного сообщения
+    div.textContent = message //вносим в него текст нового сообщения
+    div.id = 'message' //указываем для него id
+    messages.appendChild(div) //добавляем в общий контейнер
+    messages.scrollTop = messages.scrollHeight //автоскролл вниз при новых сообщениях
     /*window.scroll(0, document.body.scrollHeight)*/
 }
 
+//добавляем обработчик при нажатии на отправить
 frm.addEventListener('submit',  (event) => {
     event.preventDefault()
     if (input.value) {
@@ -30,6 +33,7 @@ socket.on('user_is_typing', () => {
     addMessage('someone is typing message...')
 })*/
 
+//слушаем сообщения с сервера от других пользователей и выводим их в чат
 socket.on('message_from_server', (message) => {
     console.log(message.hello)
     console.log(message.data)
@@ -37,5 +41,5 @@ socket.on('message_from_server', (message) => {
 })
 
 socket.on('disconnect', (message) => {
-    console.log(message.alert);
+    console.log(message.alert)
 })
