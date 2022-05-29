43de9f5b566fd033d354ea6eb3d75c3c4787305f
Matvey Mayorov <mayorovmatvey1@gmail.com>
2022-05-29 22:24:58 +0300

    removed useless packages/things and refactored

--- app.js
+++ app.js
@@ -6,86 +6,28 @@ const http = require('http')
 const bodyParser= require('body-parser')
 const { Server } = require('socket.io')
 const morgan = require('morgan')
-const hbs = require('hbs')
 const middlewares = require('./middlewares.js')
 const routers = require('./routes/routes.js')
 const helpers = require('./helpers.js')
-
 
 const app = express()
 const server = http.createServer(app)
 const io = new Server(server)
 const PORT = process.env.PORT ?? 3000
 
-app.set("view engine", "hbs")
-app.set('views', path.resolve(__dirname, 'hbs'))
-hbs.registerPartials(path.resolve(__dirname, 'hbs', 'partials'))
-
 app.use(routers.router)
 app.use(bodyParser.json()) // parse requests of content-type - application/json
 app.use(bodyParser.urlencoded({ extended: true })) // parse requests of content-type - application/x-www-form-urlencoded
 app.use(cors())
 app.use(express.static(path.resolve(__dirname, 'static')))
-app.use(express.static(path.resolve(__dirname, 'node_modules', 'bulma', 'css')))
 app.use(morgan('combined'))
 //app.use(middlewares.logger)
-
-app.get('/hello', (req,res) => {
-    res.sendFile(path.resolve(__dirname, 'static', 'hello.html'))
-})
 
 app.get('/chat', (req,res) => {
     res.sendFile(path.resolve(__dirname, 'static', 'chat.html'))
 })
 
 let users = []
-
-io.on('connection', (socket) => {
-    users.push(1)
-    console.log(colors.bgBrightGreen.black('New user connected'));
-    console.log(colors.bgBrightYellow.black(`All users = ${users.length}`))
-
-    socket.on('message_from_client', (message) => {
-        console.log(colors.bgBrightWhite.black(`Messsage: ${message}`))
-
-        socket.broadcast.emit('message_from_server', {
-            hello: "Hello NodeJS",
-            data: message
-        })
-    })
-
-  /*  socket.on('typing', () => {
-        socket.broadcast.emit('user_is_typing')
-    })*/
-
-    socket.on('disconnect', () => {
-    users.pop()
-    console.log(colors.bgBrightRed.brightWhite('user disconnected'))
-    console.log(colors.bgBrightYellow.black(`All users = ${users.length}`))
-  })
-})
-
-app.get('/', (req,res) => {
-     res.sendFile(path.resolve(__dirname, 'static', 'home.html'))
-/*     res.render('home', {
-         title: "Home Page",
-         active: "home"
-     })*/
-})
-
-app.get('/about', (req,res) => {
-    res.render('about', {
-        title: "About Page",
-        active: "about"
-    })
-})
-
-app.get('/profile', (req,res) => {
-    res.render('profile', {
-        title: "Profile Page",
-        active: "profile"
-    })
-})
 
 /*let users = [
     {id: 1, name: 'Neo', status: 'online'},
@@ -95,11 +37,26 @@ app.get('/profile', (req,res) => {
     {id: 5, name: 'Jhon Wick', status: 'offline'}
 ]*/
 
-app.get('/users', (req,res) => {
-    res.render('users', {
-        title: "Users Page",
-        users: users
+io.on('connection', (socket) => {
+    users.push(1)
+    console.log(colors.bgBrightGreen.black('New user connected'));
+    console.log(colors.bgBrightYellow.black(`All users = ${users.length}`))
+
+    socket.on('message_from_client', (message) => {
+        console.log(colors.bgBrightWhite.black(`Message: ${message}`))
+
+        socket.broadcast.emit('message_from_server', message)
     })
+    
+    socket.on('disconnect', () => {
+    users.pop()
+    console.log(colors.bgBrightRed.brightWhite('user disconnected'))
+    console.log(colors.bgBrightYellow.black(`All users = ${users.length}`))
+  })
+})
+
+app.get('/', (req,res) => {
+     res.sendFile(path.resolve(__dirname, 'static', 'home.html'))
 })
 
 app.get('/favicon.ico', (req,res) => {
@@ -114,5 +71,5 @@ app.get('/favicon.ico', (req,res) => {
 //})
 
 server.listen(PORT, () => {
-    console.log(`Server has been started on ${PORT}...`)
+    console.log(`Server has been started on http://localhost:${PORT}...`)
 })
